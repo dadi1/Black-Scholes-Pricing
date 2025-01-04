10f9f58cc8e8452e8daee035cfd5b900cfa9bb22
dadi1 <narayan.shimanoe@gmail.com>
2025-01-04 10:54:30 -0300

    function first finishing

--- src/black-scholes-dd/blackscholes.py
+++ src/black-scholes-dd/blackscholes.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import norm
 
 # Defining constants
 
@@ -21,6 +22,18 @@ def black_scholes(S , K, T, r, sigma):
     
     Return:
     - V: float -
-    - d1: float - 
-    - d2: float - 
+    - N1: float - 
+    - N1: float - 
     """
+    # Calculation for d1 a and d2
+    d1 = (np.log(S / K) + (r + 0.5 sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    # Calculation for N1 and N2
+    N1 = norm.cdf(d1)
+    N2 = norm.cdf(d2)
+
+    # v calculation
+    V = S * N1 - K * np.exp(-r * Tau) * N2
+
+    return V, N1, N2
