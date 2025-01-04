9133d6f43397549a92b90c48b47752b62504b495
dadi1 <narayan.shimanoe@gmail.com>
2025-01-04 11:05:56 -0300

    test for blackscholes.py added

--- src/black-scholes-dd/test.py
+++ src/black-scholes-dd/test.py
@@ -1,4 +1,6 @@
-from .blackscholes import *
+from blackscholes import *
+
+# Test for black_scholes() module
 
 S = 100  # Preço do ativo subjacente
 K = 100  # Preço de exercício
@@ -6,8 +8,9 @@ T = 1    # Tempo até a maturidade (em anos)
 r = 0.05 # Taxa livre de risco (5%)
 sigma = 0.2 # Volatilidade anual (20%)
 
-call_price = black_scholes(S, K, T, r, sigma, option_type="call")
-put_price = black_scholes(S, K, T, r, sigma, option_type="put")
+V, N1, N2 = black_scholes(S, K, T, r, sigma)
+print(V)
+print(N1)
+print(N2)
 
-print(f"Preço da Call: {call_price:.2f}")
-print(f"Preço da Put: {put_price:.2f}")
+# End of test for black_scholes() module
