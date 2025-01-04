862e51bde2dcab3bf614a428d8fba8e2bd72a40e
dadi1 <narayan.shimanoe@gmail.com>
2025-01-04 17:57:37 -0300

    wiener_increment() test added

--- src/black-scholes-dd/test.py
+++ src/black-scholes-dd/test.py
@@ -1,4 +1,5 @@
 from blackscholes import *
+from wienerincrement import *
 
 # Test for black_scholes() module
 
@@ -9,8 +10,16 @@ r = 0.05 # Taxa livre de risco (5%)
 sigma = 0.2 # Volatilidade anual (20%)
 
 V, N1, N2 = black_scholes(S, K, T, r, sigma)
-print(V)
-print(N1)
-print(N2)
+#print(V)
+#print(N1)
+#print(N2)
 
 # End of test for black_scholes() module
+
+# Test for wiener_increment() module
+
+St = wiener_increment(r, sigma, S)
+
+print(St)
+
+# End wiener_increment() module
