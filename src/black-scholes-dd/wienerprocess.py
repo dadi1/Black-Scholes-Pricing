41efdc380e1bc73e65f9286f5e975952663fc492
dadi1 <narayan.shimanoe@gmail.com>
2025-01-26 16:40:15 -0300

    python files removed

--- src/black-scholes-dd/wienerprocess.py
+++
@@ -1,7 +0,0 @@
-
-# Gerar o caminho de Wiener para o ativo
-np.random.seed(1355)  # Fixar a semente para reprodutibilidade
-dW = np.random.normal(0, 1, N)  # Incrementos aleatórios
-W = np.concatenate(([0], np.cumsum(dW)))  # Processo de Wiener
-R = np.exp((r - 0.5 * sig**2) * np.linspace(0, T, N + 1))  # Fator de crescimento
-St = S0 * R * np.exp(sig * W)  # Simulação de preço do ativo
