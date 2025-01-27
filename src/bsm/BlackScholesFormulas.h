d86f4a4acda1cc0ded7d7600a8188a9bff1140cd
dadi1 <narayan.shimanoe@gmail.com>
2025-01-27 11:25:36 -0300

    heading added

---
+++ src/bsm/BlackScholesFormulas.h
@@ -0,0 +1,8 @@
+/**
+ * @file BlackScholesFormulas.h
+ * @author Narayan Shimanoe Lisboa
+ * @date 27/01/2025
+ * 
+ * @brief Header File implemetation of Black-Scholes function for pricing derivatives
+ */
+
