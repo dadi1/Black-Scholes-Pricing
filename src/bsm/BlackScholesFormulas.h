9f8fde44213afe2d8cb3a5804709687fadfbd25c
dadi1 <narayan.shimanoe@gmail.com>
2025-01-28 11:14:32 -0300

    formulas added in header file

--- src/bsm/BlackScholesFormulas.h
+++ src/bsm/BlackScholesFormulas.h
@@ -6,3 +6,49 @@
  * @brief Header File implemetation of Black-Scholes function for pricing derivatives
  */
 
+/**
+ * @param Spot - Current price of the underlying asset.
+ * @param Strike - Strike price of the derivative.
+ * @param r - Annual risk-free rate.
+ * @param d -   Annual dividend yield of the underlyin asset.
+ * @param Vol - Annual Volatility of the underlying asset.
+ * @param Expiry - Expiry time of the asset in years.
+ *
+ * @return double - The price of the asset.
+ * 
+ */
+
+// Function for Call Option.
+double BlackScholesCall( double Spot,
+                          double Strike,
+                          double r,
+                          double d,
+                          double Vol,
+                          double Expiry);
+
+
+// Function for Put Option.
+double BlackScholesPut( double Spot,
+                          double Strike,
+                          double r,
+                          double d,
+                          double Vol,
+                          double Expiry);
+
+
+// Funtion for Ditial Call.
+double BlackScholesDigitalCall( double Spot,
+                          double Strike,
+                          double r,
+                          double d,
+                          double Vol,
+                          double Expiry);
+
+
+// Function for Call Vega.
+double BlackScholesCallVega( double Spot,
+                          double Strike,
+                          double r,
+                          double d,
+                          double Vol,
+                          double Expiry);
