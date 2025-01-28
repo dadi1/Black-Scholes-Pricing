e6fe0117417f1fbe918e2d6000ec4f0fd1b22b45
dadi1 <narayan.shimanoe@gmail.com>
2025-01-28 11:57:58 -0300

    Normal.h functions implemented

---
+++ src/normals/Normals.h
@@ -0,0 +1,26 @@
+/**
+ * @file BlackScholesFormulas.h
+ * @author Narayan Shimanoe Lisboa
+ * @date 28/01/2025
+ * 
+ * @brief Header file implementation for a support Function to be used in BSM model 
+ * for a cumilative normal distribution.
+ * 
+ *  @warning There will no be no much documentation for this text.
+ * 
+ */
+
+#ifndef NORMALS_H
+#define NORMALS_H
+
+double NormalDensity(double x);
+
+double CumulativeNormal(double x);
+
+double InverseCumulativeNormal(double x);
+
+#endif 
+
+
+
+
