ebac80f07eaada47d1ed976ec73e367f1197d576
dadi1 <narayan.shimanoe@gmail.com>
2025-01-28 15:52:29 -0300

    blackcholes formuals implemented in source-file

---
+++ src/bsm/BlackScholesFormulas.cpp
@@ -0,0 +1,90 @@
+/**
+ * @file BlackScholesFormulas.h
+ * @author Narayan Shimanoe Lisboa
+ * @date 28/01/2025
+ * 
+ * @brief Header File implemetation of Black-Scholes function for pricing derivatives
+ */
+
+/**
+ * @param Spot - Current price of the underlying asset.
+ * @param Strike - Strike price of the derivative.
+ * @param r - Annual risk-free rate.
+ * @param d -   Annual dividend yield of the underlyin asset.
+ * @param Vol - Annual Volatility of the underlying asset.
+ * @param Expiry - Expiry time of the asset in years.
+ *
+ * @return double - The price of the asset.
+ * 
+ */
+
+#include "BlackScholesFormulas.h"
+#include "../normals/Normals.h"
+#include <cmath>
+
+// Function for Call Option.
+double BlackScholesCall(double Spot,
+                        double Strike,
+                        double r,
+                        double d,
+                        double Vol,
+                        double Expiry)
+{
+    double standardDeviation = Vol * std::sqrt(Expiry);
+    double moneyNess = std::log(Spot / Strike);
+    double d1 = (moneyNess + (r - d) * Expiry +
+        0.5 * standardDeviation * standardDeviation) / standardDeviation;
+    double d2 = d1 - standardDeviation;
+
+    return Spot * std::exp(-d * Expiry) * CumulativeNormal(d1) -
+        Strike * std::exp(-r * Expiry) * CumulativeNormal(d2);
+}
+
+// Function for Put Option.
+double BlackScholesPut(double Spot,
+                       double Strike,
+                       double r,
+                       double d,
+                       double Vol,
+                       double Expiry)
+{
+    double standardDeviation = Vol * std::sqrt(Expiry);
+    double moneyNess = std::log(Spot / Strike);
+    double d2 = (moneyNess + (r - d) * Expiry -
+        0.5 * standardDeviation * standardDeviation) / standardDeviation;
+    
+    return std::exp(-r * Expiry) * CumulativeNormal(d2);
+}
+
+// Function for Digital Put Option.
+double BlackScholesDigitalPut(double Spot,
+                              double Strike,
+                              double r,
+                              double d,
+                              double Vol,
+                              double Expiry)
+{
+    double standardDeviation = Vol * std::sqrt(Expiry);
+    double moneyNess = std::log(Spot / Strike);
+    double d2 = (moneyNess + (r - d) * Expiry - 
+        0.5 * standardDeviation * standardDeviation) / standardDeviation;
+    
+    return std::exp(-r * Expiry) * (1.0 - CumulativeNormal(d2));
+}
+
+// Function for Call Vega Option.
+double BlackScholesCallVega(double Spot,
+                            double Strike,
+                            double r,
+                            double d,
+                            double Vol,
+                            double Expiry)
+{
+    double standardDeviation = Vol * std::sqrt(Expiry);
+    double moneyNess = std::log(Spot / Strike);
+    double d1 = (moneyNess + (r - d) * Expiry + 
+        0.5 * standardDeviation + standardDeviation) / standardDeviation;
+    
+    return Spot * std::exp(-d * Expiry) * std::sqrt(Expiry) * NormalDensity(d1);
+}
+
