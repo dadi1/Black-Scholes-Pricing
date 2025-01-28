b741ef6a79590432444ba1646d53ddb67ed11d39
dadi1 <narayan.shimanoe@gmail.com>
2025-01-28 13:30:27 -0300

    InverseCumulativeNormal(double x) test implemented

--- tests/testnormal.cpp
+++ tests/testnormal.cpp
@@ -13,8 +13,13 @@
 int main()
 {
     // test for NormalDensity(double x) function:
-    std::cout << "NormalDensity(x) are: " << std::endl;
-    std:: cout << NormalDensity(-30.3) << " " << NormalDensity(0) << " " << NormalDensity(30.3) << std::endl;
+    //std::cout << "NormalDensity(x) are: " << std::endl;
+    //std:: cout << NormalDensity(-30.3) << " " << NormalDensity(0) << " " <<
+    // NormalDensity(30.3) << std::endl;
 
+
+    //test for InverseCumulativeNormal(double x) function:
+    std::cout << "InverseCumulativeNormal(double x) function are: " << std::endl;
+    std::cout << InverseCumulativeNormal(0.43) << " " << InverseCumulativeNormal(0.41) << " " << InverseCumulativeNormal(- 0.41) << std::endl;
     return 0;
 }
