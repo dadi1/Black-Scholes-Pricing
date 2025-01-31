b6020fd3833d7ce85a93fd3bd9d3c2752042dee2
dadi1 <narayan.shimanoe@gmail.com>
2025-01-31 18:18:22 -0300

    blackscholesput() function teting implemented

--- tests/test.py
+++ tests/test.py
@@ -18,10 +18,29 @@ with parameters:
     volatility = 0.2
     Time = 1
     """
-libtest = ctypes.CDLL("./libtest.so").BlackScholesCall_c
+"""
+BlackScholesCall = ctypes.CDLL("./libtest.so").BlackScholesCall_c
 
-libtest.restype = None
-libtest.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double]
-libtest.restype = ctypes.c_double
+BlackScholesCall.restype = None
+BlackScholesCall.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double]
+BlackScholesCall.restype = ctypes.c_double
 
-print(libtest(100.0, 110.0, 0.01, 0.02, 0.2, 1.0))
+print(BlackScholesCall(100.0, 110.0, 0.01, 0.02, 0.2, 1.0)) """
+
+"""
+
+Test for BlackScholesPut() function
+with parameters:
+    Spot = 100
+    Strike = 110
+    r = 0.01
+    d = 0.02
+    volatility = 0.2
+    Time = 1
+    """
+BlackScholesPut = ctypes.CDLL("./libtest.so").BlackScholesPut_c
+
+BlackScholesPut.restype = None
+BlackScholesPut.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double]
+BlackScholesPut.restype = ctypes.c_double
+print(BlackScholesPut(100.0, 110.0, 0.01, 0.02, 0.2, 1.0))
